274f117195cee03b44eac3f2ae5fa4fe9a004e6f
Karyna <kuzhelnykova86@gmail.com>
2026-07-04 00:48:05 -0400

    Update comments in containsDuplicate method
    Clarified comments to improve code readability.

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -1,6 +1,7 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        #int nums. return true, if value twice (duplicates allowed - ) in nums[i], return false if i distinct
+        #if nums have duplicates, return True
+        #if nums distinct [no duplicates - use set], return False
         if len(nums) == len(set(nums)):
             return False
         return True
